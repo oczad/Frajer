8c5e3dbc33d1246106968fe97b35e18a75b0682d
oczad <Kbhfen2369>
2020-09-07 09:05:35 +0200

    K

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ahoj" + scanner.next());
+        System.out.println("Ahoj " + scanner.next());
     }
 }
